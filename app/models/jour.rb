5f053ad979d7e4d37b4903ee6df65dbdace46234
shingara <cyril.mougel@gmail.com>
2008-06-27 12:16:21 +0200

    clean model

--- app/models/jour.rb
+++ app/models/jour.rb
@@ -4,7 +4,6 @@ class Jour
   property :id, Integer, :key => true, :serial => true
   property :order, Integer
   property :body, Text
-  property :voyage_id, Integer, :index => true
 
   belongs_to :voyage
   
