cb0e2423675c31c585dfcfe3b6bc08585ed8d399
shingara <cyril.mougel@gmail.com>
2008-06-28 11:07:25 +0200

    add uniqueness in title of voyage

--- app/models/voyage.rb
+++ app/models/voyage.rb
@@ -2,10 +2,11 @@ class Voyage
   include DataMapper::Resource
 
   property :id, Integer, :serial => true, :key => true
-  property :title, String
+  property :title, String, :key => true
   
   has n, :jours
 
   validates_present :title
+  validates_is_unique :title
   
 end
